9391e5aef8a54f78c2b0e80793084077081162f2
Timothy Mead <timothy.mead20@gmail.com>
2023-11-03 23:20:03 +1000

    Refactor costmap to be stored as 1D array so publishing to ROS is easier. Implemented publishing of the costmap to UInt8MultiArray message

--- beboop_vision/include/potential_field_driver.hpp
+++ beboop_vision/include/potential_field_driver.hpp
@@ -1,13 +1,17 @@
 #include "vision_driver.hpp"
+#include "std_msgs/msg/u_int8_multi_array.hpp"
 
 class PotentialFieldDriver : public VisionDriver
 {
     public:
         PotentialFieldDriver();
 
-        void add_point(uint32_t x_position, uint32_t y_position, int8_t value);
         void translate(int32_t x_translation, int32_t y_translation);
         void fade(uint8_t fade_magnitude);
+        void publish();
+
+        void set_tile(uint32_t row_index, uint32_t column_index, int8_t value);
+        int8_t get_tile(uint32_t row_index, uint32_t column_index);
 
     private:
         void analyse_frame(cv::Mat image_frame) override;
@@ -16,6 +20,9 @@ class PotentialFieldDriver : public VisionDriver
         
         static constexpr uint32_t COSTMAP_WIDTH = 2000;
         static constexpr uint32_t COSTMAP_HEIGHT = 2000;
+        static constexpr uint64_t COSTMAP_LENGTH = COSTMAP_WIDTH * COSTMAP_HEIGHT;
 
-        int8_t costmap[COSTMAP_WIDTH][COSTMAP_HEIGHT];
+        int8_t costmap[COSTMAP_LENGTH];
+
+        rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr _potential_field_publisher;
 };
--- beboop_vision/src/potential_field_driver.cpp
+++ beboop_vision/src/potential_field_driver.cpp
@@ -3,6 +3,8 @@
 PotentialFieldDriver::PotentialFieldDriver() : VisionDriver("track_error_driver")
 {
     _initialise_costmap();
+
+    _potential_field_publisher = this->create_publisher<std_msgs::msg::UInt8MultiArray>("potential_field", 10);
 }
 
 void
@@ -13,22 +15,47 @@ PotentialFieldDriver::analyse_frame(cv::Mat image_frame)
 }
 
 void
-PotentialFieldDriver::_initialise_costmap()
+PotentialFieldDriver::publish()
 {
-    for (uint32_t row_index = 0; row_index < COSTMAP_WIDTH; row_index++)
-    {
-        for (uint32_t column_index = 0; column_index < COSTMAP_HEIGHT; column_index++)
-        {
-            costmap[row_index][column_index] = 0;
-        }
-    }
+    std_msgs::msg::UInt8MultiArray msg;
+
+    msg.data.clear();
+    msg.data.insert(msg.data.end(), &costmap[0], &costmap[COSTMAP_LENGTH]);
+    msg.layout.data_offset = 0;
+    msg.layout.dim[0].label = "height";
+    msg.layout.dim[0].size = COSTMAP_HEIGHT;
+    msg.layout.dim[0].stride = COSTMAP_HEIGHT * COSTMAP_WIDTH;
+    msg.layout.dim[1].label = "width";
+    msg.layout.dim[1].size = COSTMAP_WIDTH;
+    msg.layout.dim[1].stride = COSTMAP_WIDTH;
+
+    _potential_field_publisher->publish(msg);
 }
 
 
 void
-PotentialFieldDriver::add_point(uint32_t x_position, uint32_t y_position, int8_t value)
+PotentialFieldDriver::set_tile(uint32_t row_index, uint32_t column_index, int8_t value)
 {
-    costmap[x_position][y_position] = value;
+    costmap[column_index + (row_index*COSTMAP_WIDTH)] = value;
+}
+
+int8_t
+PotentialFieldDriver::get_tile(uint32_t row_index, uint32_t column_index)
+{
+    return costmap[row_index + (column_index*COSTMAP_WIDTH)];
+}
+
+
+void
+PotentialFieldDriver::_initialise_costmap()
+{
+    for (uint32_t row_index = 0; row_index < COSTMAP_HEIGHT; row_index++)
+    {
+        for (uint32_t column_index = 0; column_index < COSTMAP_WIDTH; column_index++)
+        {
+            set_tile(row_index, column_index, 0);
+        }
+    }
 }
 
 void
@@ -40,23 +67,23 @@ PotentialFieldDriver::translate(int32_t x_translation, int32_t y_translation)
         return;
     }
 
-    for (uint32_t row_index = 0; row_index < COSTMAP_WIDTH; row_index++)
+    for (uint32_t row_index = 0; row_index < COSTMAP_HEIGHT; row_index++)
     {
-        for (uint32_t column_index = 0; column_index < COSTMAP_HEIGHT; column_index++)
+        for (uint32_t column_index = 0; column_index < COSTMAP_WIDTH; column_index++)
         {
             int64_t old_row_index = row_index + x_translation;
             int64_t old_column_index = column_index + y_translation;
 
-            if ((old_row_index > COSTMAP_WIDTH)||
+            if ((old_row_index > COSTMAP_HEIGHT)||
                 (old_row_index < 0) ||
-                (old_column_index > COSTMAP_HEIGHT) ||
+                (old_column_index > COSTMAP_WIDTH) ||
                 (old_column_index < 0))
             {
-                costmap[row_index][column_index] = 0;
+                set_tile(row_index, column_index, 0);
             }
             else
             {
-                costmap[row_index][column_index] = costmap[old_row_index][old_column_index];
+                set_tile(row_index, column_index, get_tile(old_row_index, old_column_index));
             }
         }
     }
@@ -66,13 +93,12 @@ PotentialFieldDriver::translate(int32_t x_translation, int32_t y_translation)
 void
 PotentialFieldDriver::fade(uint8_t fade_magnitude)
 {
-    for (uint32_t row_index = 0; row_index < COSTMAP_WIDTH; row_index++)
+    for (uint32_t row_index = 0; row_index < COSTMAP_HEIGHT; row_index++)
     {
-        for (uint32_t column_index = 0; column_index < COSTMAP_HEIGHT; column_index++)
+        for (uint32_t column_index = 0; column_index < COSTMAP_WIDTH; column_index++)
         {
-            int8_t old_val = costmap[row_index][column_index];
-
-            costmap[row_index][column_index] = signbit(old_val) * std::max(old_val - fade_magnitude, 0);
+            int8_t old_val = get_tile(row_index, column_index);
+            set_tile(row_index, column_index, signbit(old_val) * std::max(old_val - fade_magnitude, 0));
         }
     }
 }
