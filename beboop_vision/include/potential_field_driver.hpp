b125ec1c70ff4f1b47e2314395f9b336ce44ca40
Timothy Mead <timothy.mead20@gmail.com>
2023-11-04 19:44:49 +1000

    Added conversion from pixels to m for the occupancy grid

--- beboop_vision/include/potential_field_driver.hpp
+++ beboop_vision/include/potential_field_driver.hpp
@@ -16,14 +16,18 @@ class PotentialFieldDriver : public VisionDriver
         int8_t get_tile(uint32_t row_index, uint32_t column_index);
         void clear();
 
+        // Converts a point in pixels to a point in meters using a calibrated empirical model
+        void pixels_to_m(float x_px, float y_px, float &x_m, float &y_m);
+
     private:
         void analyse_frame(cv::Mat image_frame) override;
 
         void _initialise_costmap();
         
-        static constexpr uint32_t COSTMAP_WIDTH = 100;
-        static constexpr uint32_t COSTMAP_HEIGHT = 100;
+        static constexpr uint32_t COSTMAP_WIDTH = 200;
+        static constexpr uint32_t COSTMAP_HEIGHT = 200;
         static constexpr uint64_t COSTMAP_LENGTH = COSTMAP_WIDTH * COSTMAP_HEIGHT;
+        static constexpr float COSTMAP_RESOLUTION = 0.05;   // [m/cell]
 
         int8_t costmap[COSTMAP_LENGTH];
 
--- beboop_vision/src/potential_field_driver.cpp
+++ beboop_vision/src/potential_field_driver.cpp
@@ -1,4 +1,5 @@
 #include "potential_field_driver.hpp"
+#include <math.h>
 
 PotentialFieldDriver::PotentialFieldDriver() : VisionDriver("track_error_driver")
 {
@@ -22,6 +23,9 @@ PotentialFieldDriver::analyse_frame(cv::Mat image_frame)
 	cv::medianBlur(yellow_frame, yellow_frame, 7);
 	cv::medianBlur(blue_frame, blue_frame, 7);
 
+    cv::imshow("Combined Threshold", blue_frame + yellow_frame);
+	cv::waitKey(1);
+
     clear();
     add_bin_image_to_occupancy(yellow_frame);
     add_bin_image_to_occupancy(blue_frame);
@@ -38,17 +42,53 @@ PotentialFieldDriver::add_bin_image_to_occupancy(cv::Mat binary_image)
     std::vector<cv::Point> locations;
     cv::findNonZero(binary_image, locations);
 
-    RCLCPP_INFO(this->get_logger(), "num pix: [%d]", locations.size());
-
-    for (cv::Point point : locations)
+    for (cv::Point point_px : locations)
     {
-        uint32_t row_index = scale(point.y, 0, binary_image.rows, 0, COSTMAP_HEIGHT-1);
-        uint32_t column_index = scale(point.x, 0, binary_image.cols, 0, COSTMAP_WIDTH-1);
+        float x_m, y_m;
+        pixels_to_m(point_px.x, point_px.y, x_m, y_m);
+
+        uint32_t row_index = std::round(x_m / COSTMAP_RESOLUTION);
+        uint32_t column_index = std::round(y_m / COSTMAP_RESOLUTION) + (COSTMAP_WIDTH/2);
+
+        // Ignore readings that go outside our defined costmap range
+        if (row_index > COSTMAP_HEIGHT || column_index > COSTMAP_WIDTH)
+        {
+            RCLCPP_INFO(this->get_logger(), "Out of bounds: [%f, %f]", x_m, y_m);
+            return;
+        }
 
         uint8_t new_value = get_tile(row_index, column_index) + 80;
         set_tile(row_index, column_index, new_value);
     }
+
+    // Uncomment for data collection
+    // if (locations.size() != 0)
+    // {
+    //     cv::Point mid = locations[locations.size()/2];
+
+    //     RCLCPP_INFO(this->get_logger(), "mid x: [%d]", mid.x);
+    //     RCLCPP_INFO(this->get_logger(), "mid y: [%d]", mid.y);
+    // }
 }
+
+void
+PotentialFieldDriver::pixels_to_m(float x_px, float y_px, float &x_m, float &y_m)
+{
+    static constexpr float y_pixel_to_distance_a = 269.0;
+    static constexpr float y_pixel_to_distance_b = -300.64;
+    static constexpr float x_pixel_to_bearing_a = 0.0850541;
+    static constexpr float x_pixel_to_bearing_b = -52.4552;
+
+    // Convert pixel coordinates into real world distance and bearing
+    // using empirical model.
+    float forward_distance_m = y_pixel_to_distance_a / (static_cast<float>(y_px) + y_pixel_to_distance_b);
+    float bearing_deg = (x_pixel_to_bearing_a * static_cast<float>(x_px)) + x_pixel_to_bearing_b;
+
+    // Using FLU frame
+    x_m = forward_distance_m;
+    y_m = forward_distance_m * std::tan(bearing_deg * M_PI / 180.0);
+}
+
 
 uint32_t
 PotentialFieldDriver::scale(uint32_t value, uint32_t old_min, uint32_t old_max, uint32_t new_min, uint32_t new_max)
@@ -63,8 +103,7 @@ PotentialFieldDriver::publish()
 
     msg.header.stamp = rclcpp::Node::now();
     msg.header.frame_id = "map";
-    float costmap_resolution = 0.05;
-    msg.info.resolution = costmap_resolution; // [m/cell]
+    msg.info.resolution = COSTMAP_RESOLUTION;
     msg.info.width = COSTMAP_WIDTH;
     msg.info.height = COSTMAP_HEIGHT;
     msg.info.origin.position.x = 0;
