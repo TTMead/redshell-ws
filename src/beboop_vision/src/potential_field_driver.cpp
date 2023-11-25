528c3a58c07bdc7ad477feaf228ba3d7d1d4d348
Timothy Mead <timothy.mead20@gmail.com>
2023-11-25 20:10:37 +1000

    Separated occupancy grid data logic into isolated header file

--- src/beboop_vision/src/potential_field_driver.cpp
+++ src/beboop_vision/src/potential_field_driver.cpp
@@ -1,9 +1,15 @@
-#include "potential_field_driver.hpp"
 #include <math.h>
+
+#include "potential_field_driver.hpp"
+#include "occupancy_grid_helpers.h"
+
+
 
 PotentialFieldDriver::PotentialFieldDriver() : VisionDriver("track_error_driver")
 {
-    _initialise_costmap();
+    initialise_occupancy_grid_msg();
+    initialise_occupancy_values(_occupancy_grid);
+
 
     _potential_field_publisher = this->create_publisher<nav_msgs::msg::OccupancyGrid>("potential_field", 10);
 
@@ -11,6 +17,21 @@ PotentialFieldDriver::PotentialFieldDriver() : VisionDriver("track_error_driver"
     this->declare_parameter("y_pixel_to_distance_b", -300.64);
     this->declare_parameter("x_pixel_to_bearing_a", 0.0850541);
     this->declare_parameter("x_pixel_to_bearing_b", -52.4552);
+}
+
+void
+PotentialFieldDriver::initialise_occupancy_grid_msg()
+{
+    _occupancy_grid.info.resolution = COSTMAP_RESOLUTION;
+    _occupancy_grid.info.width = COSTMAP_WIDTH;
+    _occupancy_grid.info.height = COSTMAP_HEIGHT;
+    _occupancy_grid.info.origin.position.x = 0;
+    _occupancy_grid.info.origin.position.y = 0;
+    _occupancy_grid.info.origin.position.z = 0;
+    _occupancy_grid.info.origin.orientation.x = 0;
+    _occupancy_grid.info.origin.orientation.y = 0;
+    _occupancy_grid.info.origin.orientation.z = 0;
+    _occupancy_grid.info.origin.orientation.w = 1;
 }
 
 void
@@ -40,7 +61,7 @@ PotentialFieldDriver::analyse_frame(cv::Mat image_frame)
         cv::waitKey(1);
     }
 
-    clear();
+    clear(_occupancy_grid);
     add_bin_image_to_occupancy(track_frame);
 
     publish();
@@ -66,8 +87,8 @@ PotentialFieldDriver::add_bin_image_to_occupancy(cv::Mat binary_image)
             continue;
         }
 
-        uint8_t new_value = get_tile(row_index, column_index) + 80;
-        set_tile(row_index, column_index, new_value);
+        uint8_t new_value = get_tile(_occupancy_grid, row_index, column_index) + 80;
+        set_tile(_occupancy_grid, row_index, column_index, new_value);
     }
 
     // Uncomment for data collection
@@ -108,130 +129,10 @@ PotentialFieldDriver::scale(uint32_t value, uint32_t old_min, uint32_t old_max, 
 void
 PotentialFieldDriver::publish()
 {
-    nav_msgs::msg::OccupancyGrid msg;
+    _occupancy_grid.header.stamp = rclcpp::Node::now();
+    _occupancy_grid.header.frame_id = "map";
 
-    msg.header.stamp = rclcpp::Node::now();
-    msg.header.frame_id = "map";
-    msg.info.resolution = COSTMAP_RESOLUTION;
-    msg.info.width = COSTMAP_WIDTH;
-    msg.info.height = COSTMAP_HEIGHT;
-    msg.info.origin.position.x = 0;
-    msg.info.origin.position.y = 0;
-    msg.info.origin.position.z = 0;
-    msg.info.origin.orientation.x = 0;
-    msg.info.origin.orientation.y = 0;
-    msg.info.origin.orientation.z = 0;
-    msg.info.origin.orientation.w = 1;
-
-    msg.data.clear();
-    msg.data.insert(msg.data.end(), &costmap[0], &costmap[COSTMAP_LENGTH]);
-
-    _potential_field_publisher->publish(msg);
-}
-
-
-void
-PotentialFieldDriver::set_tile(uint32_t row_index, uint32_t column_index, int8_t value)
-{
-    if (row_index >= COSTMAP_HEIGHT)
-    {
-        RCLCPP_ERROR(this->get_logger(), "Attempt to access out of bounds row. Requested row: %d. Max height: %d.", row_index, COSTMAP_HEIGHT);
-        return;
-    }
-
-    if (column_index >= COSTMAP_WIDTH)
-    {
-        RCLCPP_ERROR(this->get_logger(), "Attempt to access out of bounds column. Requested column: %d. Max width: %d.", column_index, COSTMAP_WIDTH);
-        return;
-    }
-
-    costmap[column_index + (row_index*COSTMAP_WIDTH)] = value;
-}
-
-int8_t
-PotentialFieldDriver::get_tile(uint32_t row_index, uint32_t column_index)
-{
-    if (row_index >= COSTMAP_HEIGHT)
-    {
-        RCLCPP_ERROR(this->get_logger(), "Attempt to access out of bounds row. Requested row: %d. Max height: %d.", row_index, COSTMAP_HEIGHT);
-        return 0;
-    }
-
-    if (column_index >= COSTMAP_WIDTH)
-    {
-        RCLCPP_ERROR(this->get_logger(), "Attempt to access out of bounds column. Requested column: %d. Max width: %d.", column_index, COSTMAP_WIDTH);
-        return 0;
-    }
-
-    return costmap[row_index + (column_index*COSTMAP_WIDTH)];
-}
-
-
-void
-PotentialFieldDriver::_initialise_costmap()
-{
-    for (uint32_t row_index = 0; row_index < COSTMAP_HEIGHT; row_index++)
-    {
-        for (uint32_t column_index = 0; column_index < COSTMAP_WIDTH; column_index++)
-        {
-            set_tile(row_index, column_index, 0);
-        }
-    }
-}
-
-void
-PotentialFieldDriver::translate(int32_t x_translation, int32_t y_translation)
-{
-    if ((abs(x_translation) > COSTMAP_WIDTH/2) || (abs(y_translation) > COSTMAP_HEIGHT/2))
-    {
-        RCLCPP_ERROR(this->get_logger(), "Translation magnitude too high [%d, %d]", x_translation, y_translation);
-        return;
-    }
-
-    for (uint32_t row_index = 0; row_index < COSTMAP_HEIGHT; row_index++)
-    {
-        for (uint32_t column_index = 0; column_index < COSTMAP_WIDTH; column_index++)
-        {
-            int64_t old_row_index = row_index + x_translation;
-            int64_t old_column_index = column_index + y_translation;
-
-            if ((old_row_index > COSTMAP_HEIGHT)||
-                (old_row_index < 0) ||
-                (old_column_index > COSTMAP_WIDTH) ||
-                (old_column_index < 0))
-            {
-                set_tile(row_index, column_index, 0);
-            }
-            else
-            {
-                set_tile(row_index, column_index, get_tile(old_row_index, old_column_index));
-            }
-        }
-    }
-}
-
-
-void
-PotentialFieldDriver::clear()
-{
-    for (uint32_t i = 0; i < COSTMAP_LENGTH; i++)
-    {
-        costmap[i] = 0;
-    }
-}
-
-
-void
-PotentialFieldDriver::fade(uint8_t fade_magnitude)
-{
-    for (uint32_t row_index = 0; row_index < COSTMAP_HEIGHT; row_index++)
-    {
-        for (uint32_t column_index = 0; column_index < COSTMAP_WIDTH; column_index++)
-        {
-            int8_t old_val = get_tile(row_index, column_index);
-            set_tile(row_index, column_index, signbit(old_val) * std::max(old_val - fade_magnitude, 0));
-        }
-    }
+    _potential_field_publisher->publish(_occupancy_grid);
 }
 
 
