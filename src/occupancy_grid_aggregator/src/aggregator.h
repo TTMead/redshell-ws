1b826901e2b83cadca9c8c07c38a652f80758182
Timothy Mead <timothy.mead20@gmail.com>
2024-02-18 21:04:30 +1000

    Fixes to offset handling

--- src/occupancy_grid_aggregator/src/aggregator.h
+++ src/occupancy_grid_aggregator/src/aggregator.h
@@ -36,7 +36,7 @@ class Aggregator : public rclcpp::Node
 
             geometry_msgs::msg::PointStamped tile_location{};
             tile_location.point.x = grid.info.origin.position.x + (col * grid.info.resolution);
-            tile_location.point.y = grid.info.origin.position.x + (row * grid.info.resolution);
+            tile_location.point.y = grid.info.origin.position.y + (row * grid.info.resolution);
             tile_location.header.frame_id = grid.header.frame_id;
 
             return tile_location;
@@ -52,13 +52,16 @@ class Aggregator : public rclcpp::Node
          */
         static inline void add_point_to_grid(nav_msgs::msg::OccupancyGrid& grid, const geometry_msgs::msg::Point& point, const int8_t value)
         {
-            if ((point.x < 0) || (point.y < 0))
+            const double x_rel = point.x - grid.info.origin.position.x;
+            const double y_rel = point.y - grid.info.origin.position.y;
+
+            if ((x_rel < 0) || (y_rel < 0))
             {
                 return;
             }
 
-            const uint32_t col = std::floor(point.x / grid.info.resolution);
-            const uint32_t row = std::floor(point.y / grid.info.resolution);
+            const uint32_t col = std::floor(x_rel / grid.info.resolution);
+            const uint32_t row = std::floor(y_rel / grid.info.resolution);
 
             if ((col >= grid.info.width) || (row >= grid.info.height))
             {
