9c9582532f66fe95458c1c4f2ea1d0166c7f74f5
Timothy Mead <timothy.mead20@gmail.com>
2024-03-24 19:13:07 +1000

    Convert xml launch files into python

---
+++ src/redshell_bringup/launch/vs_sitl.launch.py
@@ -0,0 +1,34 @@
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+import launch_ros.actions
+import os
+import yaml
+from launch.substitutions import EnvironmentVariable
+import pathlib
+import launch.actions
+from launch.actions import DeclareLaunchArgument
+
+def generate_launch_description():
+    return LaunchDescription([
+        # ==== Unity Interfacing ====
+        launch_ros.actions.Node(
+            package='ros_tcp_endpoint',
+            executable='default_server_endpoint',
+            parameters=[{"ROS_IP": "127.0.0.1"}, {"ROS_TCP_PORT": 10000}],
+        ),
+
+        # ==== Vision ====
+        launch_ros.actions.Node(
+            package='redshell_vision',
+            executable='track_error_driver',
+            parameters=[{"is_sitl": True}],
+        ),
+
+        # ==== State Estimation ====
+        launch_ros.actions.Node(
+            package='redshell_bringup',
+            executable='vs_control',
+            output='screen',
+            parameters=[{"kp": 0.35},{"speed": 0.3}],
+           ),
+    ])
